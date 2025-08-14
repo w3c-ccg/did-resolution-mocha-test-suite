5d4cae12f8d9d0798f1c95e9f93559e15313ae73
BigBlueHat <byoung@bigbluehat.com>
2025-08-14 14:19:01 -0400

    Initial commit

---
+++ tests/10-test-example.js
@@ -0,0 +1,33 @@
+/*!
+ * Copyright (c) 2025 Digital Bazaar, Inc.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+import * as chai from 'chai';
+import {filterByTag} from 'vc-test-suite-implementations';
+import {helpers} from 'mocha-w3c-interop-reporter';
+
+const should = chai.should();
+
+const tag = 'BitstringStatusList';
+const {match} = filterByTag({tags: [tag]});
+
+describe('Example Test Suite', function() {
+  helpers.setupMatrix.call(this, match);
+  for(const [name] of match) {
+    describe(name, function() {
+      beforeEach(helpers.setupRow);
+      it('should pass a basic test', function() {
+        const expected = 42;
+        const actual = 40 + 2;
+        actual.should.equal(expected);
+      });
+
+      it('should fail a basic test', function() {
+        const expected = 42;
+        const actual = 40 + 1; // This will fail
+        actual.should.equal(expected);
+      });
+    });
+  }
+});
